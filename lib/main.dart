4e73a95239d0c5c3cf0beebcba3e1c75967dde53
yasmine khedr <yasmeenkhedr23@gmail.com>
2024-08-25 23:52:39 +0300

    change theme and create notes voew

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -12,6 +13,8 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      
+     theme: ThemeData.dark(),
+     home: NotesView(),
       );
   }
 }
